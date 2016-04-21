b2366bbbb829514f7c1c9cd689b31c65fe260684
Matt Payne <Payne@MattPayne.org>
2016-04-21 16:21:31 -0500

    this works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    # Fix from http://stackoverflow.com/questions/28386773/rails-activemodelforbiddenattributeserror-devise-omniauth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
